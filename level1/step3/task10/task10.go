af7a410dd2321802e6a07948783184a74b3777b1
Галиуллин Дмитрий Олегович <d.galiullin@egov66.ru>
2026-01-16 12:56:54 +0500

    Level 1 - Step 3 - Task 10: Вы поедете на бал?

---
+++ level1/step3/task10/task10.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func getText(reader *bufio.Reader) (string, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return "", err
+	}
+
+	return strings.TrimSpace(line), nil
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		text, err := getText(reader)
+		if err != nil {
+			fmt.Printf("ошибка: %s\n", err)
+			return
+		}
+
+		if text == "" {
+			break
+		}
+
+		switch text {
+		case "да", "нет", "чёрный", "белый":
+			fmt.Println("Поражение")
+		default:
+			fmt.Println("Игра продолжается")
+		}
+	}
+}
