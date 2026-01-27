d80d8781a787f8906eeb7f1c81d58ebd5b83e5f8
Галиуллин Дмитрий Олегович <d.galiullin@egov66.ru>
2026-01-27 13:14:32 +0500

    Level 6 - Step 1 - Task 8: Загруженность дорог.

---
+++ level6/step1/task8/task8.go
@@ -0,0 +1,34 @@
+package main
+
+type WeatherCondition int
+
+const (
+	Clear WeatherCondition = iota
+	Rain
+	HeavyRain
+	Snow
+)
+
+type WeatherData struct {
+	Condition WeatherCondition
+	WindSpeed int
+}
+
+func GetWeatherMultiplier(weather WeatherData) float64 {
+	multiplier := 1.0
+
+	switch weather.Condition {
+	case Rain:
+		multiplier += 0.125
+	case HeavyRain:
+		multiplier += 0.2
+	case Snow:
+		multiplier += 0.15
+	}
+
+	if weather.WindSpeed > 15 {
+		multiplier += 0.125
+	}
+
+	return multiplier
+}
